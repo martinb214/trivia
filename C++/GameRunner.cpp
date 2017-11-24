8ac4a7e8c2fc311168310c5dd89c607f5dc392f4
martinb214 <martin@Bakos-MacBook-Pro.local>
2017-11-24 21:59:53 +0100

    Add new test case to GameRunner.cpp.

--- C++/GameRunner.cpp
+++ C++/GameRunner.cpp
@@ -1,6 +1,9 @@
 ﻿#include "Game.h"
 
 int main(){
+	//Original repo's behavior
+	//Test for normal number of players
+	//Waiting: Normal execute without error
 	Game aGame;
 
 	aGame.add("Chet");
@@ -8,6 +11,15 @@ int main(){
 	aGame.add("Sue");
 
 	aGame.playGame();
-	
+
+	//Test for one player
+	//Waiting: Can't start game, getting error message
+	/*
+	Game bGame;
+
+	bGame.add("Bob");
+
+	bGame.playGame();
+	*/
 	return 0;
 }
