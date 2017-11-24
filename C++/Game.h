de1b823ab9c14b5d943ab3b1eebbcae08678c29e
martinb214 <martin@Bakos-MacBook-Pro.local>
2017-11-24 20:29:15 +0100

    Make Player.hxx for Player class.

--- C++/Game.h
+++ C++/Game.h
@@ -2,34 +2,10 @@
 #define GAME_H_
 
 #include <iostream>
-#include <stdlib.h>
 #include <list>
 #include <vector>
 
-class Player{
-	private:
-		const std::string name;
-		int place;
-		int purse;
-		bool inPenaltyBox;
-	public:
-		Player(std::string newName): name(newName), place(0), purse(0), inPenaltyBox(false) {};
-		
-		const std::string& getName() {return name; };
-		const int& getPlace() {return place; };
-		const int& getPurse() {return purse; };
-		const bool& getInPenaltyBox() {return inPenaltyBox; };
-
-		void setPlace(int newPlace) {place = newPlace; if(place > 11) place -= 12; };
-		void setPurse() {purse++; };
-		void setInPenaltyBox(){
-			if(inPenaltyBox) inPenaltyBox = false;
-			else inPenaltyBox = true; 
-		};
-
-		int roll() { return (rand() % 5 + 1); };
-		int answer() {return (rand() % 9); };
-};
+#include "Player.hxx"
 
 class Game{
   private:
---
+++ C++/Player.hxx
@@ -0,0 +1,32 @@
+#ifndef PLAYER_HXX_
+#define PLAYER_HXX_
+
+#include <iostream>
+#include <stdlib.h>
+
+class Player{
+	private:
+		const std::string name;
+		int place;
+		int purse;
+		bool inPenaltyBox;
+	public:
+		Player(std::string newName): name(newName), place(0), purse(0), inPenaltyBox(false) {};
+		
+		const std::string& getName() {return name; };
+		const int& getPlace() {return place; };
+		const int& getPurse() {return purse; };
+		const bool& getInPenaltyBox() {return inPenaltyBox; };
+
+		void setPlace(int newPlace) {place = newPlace; if(place > 11) place -= 12; };
+		void setPurse() {purse++; };
+		void setInPenaltyBox(){
+			if(inPenaltyBox) inPenaltyBox = false;
+			else inPenaltyBox = true; 
+		};
+
+		int roll() { return (rand() % 5 + 1); };
+		int answer() {return (rand() % 9); };
+};
+
+#endif /* PLAYER_HXX_ */
