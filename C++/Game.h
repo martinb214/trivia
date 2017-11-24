35c97d39efdcf77b9471add121411cb1f162ae47
martinb214 <martin@Bakos-MacBook-Pro.local>
2017-11-24 16:19:42 +0100

    Define Player class in header file.

--- C++/Game.h
+++ C++/Game.h
@@ -5,21 +5,38 @@
 #include <list>
 #include <vector>
 
+class Player{
+	private:
+		const std::string name;
+		int place;
+		int purse;
+		bool inPenaltyBox;
+	public:
+		Player(std::string newName): name(newName), place(0), purse(0), inPenaltyBox(false) {};
+		
+		const std::string& getName() {return name; };
+		const int& getPlace() {return place; };
+		const int& getPurse() {return purse; };
+		const bool& getInPenaltyBox() {return inPenaltyBox; };
+
+		void setPlace(int newPlace) {place = newPlace; };
+		void setPurse() {purse++; };
+		void setInPenaltyBox(){
+			if(inPenaltyBox) inPenaltyBox = false;
+			else inPenaltyBox = true; 
+		};
+};
+
 class Game{
   private:
-	std::vector<std::string> players;
-
-	int places[6];
-	int purses[6];
-
-	bool inPenaltyBox[6];
+	std::vector<Player> players;
+	int currentPlayer;
 
 	std::list<std::string> popQuestions;
 	std::list<std::string> scienceQuestions;
 	std::list<std::string> sportsQuestions;
 	std::list<std::string> rockQuestions;
 
-	int currentPlayer;
 	bool isGettingOutOfPenaltyBox;
 
 	void askQuestion();
